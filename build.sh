3a72d4b8473b59abdf3b3497a825708b51937514
v0lk3n <volken_haxor@protonmail.com>
2026-03-22 22:48:19 +0100

    Disable building module

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 pushd `dirname $0` > /dev/null
 
-echo Building module
-cd src/module 
-make $1
+#Disabled as builded inline with kernel
+#echo Building module
+#cd src/module 
+#make $1
 
 echo Building userpace tools
 cd ../
